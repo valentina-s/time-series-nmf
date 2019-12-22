4e7daab9ae21484b6a816381aab45047dcac1cb5
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2019-12-22 01:54:06 -0800

    n_components

--- ssnmf/ssnmf.py
+++ ssnmf/ssnmf.py
@@ -7,11 +7,9 @@ import numbers
 class smoothNMF():
 
     """
-    Algorithm for NMF with eucidian norm as objective function and
+    Algorithm for NMF with Eucidian norm as objective function and
     L1 constraint on W for sparse paterns and Tikhonov regularization
     for smooth activation coefficients.
-
-    [W, H, objective] = sparse_nmf(v, params)
 
     Objective function:
     || W * H - X ||² + lambda * || W ||_1 + eta || H T ||² + betaW ||W||²
@@ -35,7 +33,7 @@ class smoothNMF():
                        (default: 1; if equal to 0, seed is not set)
          init_W:   initial setting for W (default: random;
                                           either init_w or r have to be set)
-         r: (K)       # basis functions (default: based on init_w's size;
+         n_components: (K)       # basis functions (default: based on init_w's size;
                                       either init_w or r have to be set)
          init_H:   initial setting for H (default: random)
          init_W:   initial setting for W (default: random)
@@ -62,7 +60,7 @@ class smoothNMF():
 # this is the case with no sparsity and smoothness
 
     def __init__(self, sparsity=0, smoothness=0, early_stopping=0,
-    gamma1=1.001, gamma2=1.001, betaH=0, betaW=0, r=5, max_iter=200):
+    gamma1=1.001, gamma2=1.001, betaH=0, betaW=0, n_components=5, max_iter=200):
 
         self.max_iter = max_iter
         self.sparsity = sparsity
@@ -76,14 +74,14 @@ class smoothNMF():
             self.gamma2 = gamma2
         self.betaH = betaH
         self.betaW = betaW
-        self.r = r
+        self.n_components = n_components
 
 
     def fit(self, X, W=None, H=None, init=None):
 
         [W, H, obj] = smooth_nmf(X, sparsity=self.sparsity, smoothness=self.smoothness,
             early_stopping=self.early_stopping, gamma1=self.gamma1, gamma2=self.gamma2,
-            betaH=self.betaH, betaW=self.betaW, r=self.r, max_iter=self.max_iter, W=W, H=H, init=init)
+            betaH=self.betaH, betaW=self.betaW, n_components=self.n_components, max_iter=self.max_iter, W=W, H=H, init=init)
 
         self.W = W
         self.H = H
@@ -195,7 +193,7 @@ def check_random_state(seed):
                      ' instance' % seed)
 
 
-def _initialize(X, W, H, r, init=None, eps=1e-6, random_state=None):
+def _initialize(X, W, H, n_components, init=None, eps=1e-6, random_state=None):
     #if W is None:
     #    W = np.random.uniform(0, 1, size=(X.shape[0], r))
     #if H is None:
@@ -203,67 +201,67 @@ def _initialize(X, W, H, r, init=None, eps=1e-6, random_state=None):
     #return(W, H)
 
     if init == 'random':
-        W = np.random.uniform(0, 1, size=(X.shape[0], r))
-        H = np.random.uniform(0, 1, size=(r, X.shape[1]))
-
-    # NNDSVD initialization
-    # code from
-    # https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/decomposition/_nmf.py
-    U, S, V = LA.svd(X, full_matrices=False)
-    U = U[:, :r]
-    V = V[:r, :]
-    W, H = np.zeros(U.shape), np.zeros(V.shape)
-
-    # The leading singular triplet is non-negative
-    # so it can be used as is for initialization.
-    W[:, 0] = np.sqrt(S[0]) * np.abs(U[:, 0])
-    H[0, :] = np.sqrt(S[0]) * np.abs(V[0, :])
-
-    for j in range(1, r):
-        x, y = U[:, j], V[j, :]
-
-        # extract positive and negative parts of column vectors
-        x_p, y_p = np.maximum(x, 0), np.maximum(y, 0)
-        x_n, y_n = np.abs(np.minimum(x, 0)), np.abs(np.minimum(y, 0))
-
-        # and their norms
-        x_p_nrm, y_p_nrm = LA.norm(x_p), LA.norm(y_p)
-        x_n_nrm, y_n_nrm = LA.norm(x_n), LA.norm(y_n)
-
-        m_p, m_n = x_p_nrm * y_p_nrm, x_n_nrm * y_n_nrm
-
-        # choose update
-        if m_p > m_n:
-            u = x_p / x_p_nrm
-            v = y_p / y_p_nrm
-            sigma = m_p
-        else:
-            u = x_n / x_n_nrm
-            v = y_n / y_n_nrm
-            sigma = m_n
-
-        lbd = np.sqrt(S[j] * sigma)
-        W[:, j] = lbd * u
-        H[j, :] = lbd * v
-
-    W[W < eps] = 0
-    H[H < eps] = 0
-
-    if init == "nndsvd":
-        pass
-    elif init == "nndsvda":
-        avg = X.mean()
-        W[W == 0] = avg
-        H[H == 0] = avg
-    elif init == "nndsvdar":
-        rng = check_random_state(random_state)
-        avg = X.mean()
-        W[W == 0] = abs(avg * rng.randn(len(W[W == 0])) / 100)
-        H[H == 0] = abs(avg * rng.randn(len(H[H == 0])) / 100)
+        W = np.random.uniform(0, 1, size=(X.shape[0], n_components))
+        H = np.random.uniform(0, 1, size=(n_components, X.shape[1]))
     else:
-        raise ValueError(
-            'Invalid init parameter: got %r instead of one of %r' %
-            (init, (None, 'random', 'nndsvd', 'nndsvda', 'nndsvdar')))
+        # NNDSVD initialization
+        # code from
+        # https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/decomposition/_nmf.py
+        U, S, V = LA.svd(X, full_matrices=False)
+        U = U[:, :n_components]
+        V = V[:n_components, :]
+        W, H = np.zeros(U.shape), np.zeros(V.shape)
+
+        # The leading singular triplet is non-negative
+        # so it can be used as is for initialization.
+        W[:, 0] = np.sqrt(S[0]) * np.abs(U[:, 0])
+        H[0, :] = np.sqrt(S[0]) * np.abs(V[0, :])
+
+        for j in range(1, n_components):
+            x, y = U[:, j], V[j, :]
+
+            # extract positive and negative parts of column vectors
+            x_p, y_p = np.maximum(x, 0), np.maximum(y, 0)
+            x_n, y_n = np.abs(np.minimum(x, 0)), np.abs(np.minimum(y, 0))
+
+            # and their norms
+            x_p_nrm, y_p_nrm = LA.norm(x_p), LA.norm(y_p)
+            x_n_nrm, y_n_nrm = LA.norm(x_n), LA.norm(y_n)
+
+            m_p, m_n = x_p_nrm * y_p_nrm, x_n_nrm * y_n_nrm
+
+            # choose update
+            if m_p > m_n:
+                u = x_p / x_p_nrm
+                v = y_p / y_p_nrm
+                sigma = m_p
+            else:
+                u = x_n / x_n_nrm
+                v = y_n / y_n_nrm
+                sigma = m_n
+
+            lbd = np.sqrt(S[j] * sigma)
+            W[:, j] = lbd * u
+            H[j, :] = lbd * v
+
+        W[W < eps] = 0
+        H[H < eps] = 0
+
+        if init == "nndsvd":
+            pass
+        elif init == "nndsvda":
+            avg = X.mean()
+            W[W == 0] = avg
+            H[H == 0] = avg
+        elif init == "nndsvdar":
+            rng = check_random_state(random_state)
+            avg = X.mean()
+            W[W == 0] = abs(avg * rng.randn(len(W[W == 0])) / 100)
+            H[H == 0] = abs(avg * rng.randn(len(H[H == 0])) / 100)
+        else:
+            raise ValueError(
+                'Invalid init parameter: got %r instead of one of %r' %
+                (init, (None, 'random', 'nndsvd', 'nndsvda', 'nndsvdar')))
 
     return W, H
 
@@ -277,17 +275,17 @@ def _objective_function(X, W, H, sparsity, smoothness, betaW, betaH, T=None):
     return(objective)
 
 
-def smooth_nmf(X, W, H, r=None, init=None, sparsity=0, smoothness=0, early_stopping=0,
+def smooth_nmf(X, W, H, n_components=None, init=None, sparsity=0, smoothness=0, early_stopping=0,
     gamma1=1.001, gamma2=1.001, betaH=0.1, betaW=0.1, max_iter=100,
     TTp=None, TTp_norm=None):
 
-    if r == None:
+    if n_components is None:
         r = min(X.shape[0], X.shape[1])
 
     if init == 'custom':
         pass # need to add checks for None, or option to set one
     else:
-        W, H = _initialize(X, W, H, r, init=init, eps=1e-6, random_state=None)
+        W, H = _initialize(X, W, H, n_components, init=init, eps=1e-6, random_state=None)
 
     obj = []  # list storing the objective function value
 
